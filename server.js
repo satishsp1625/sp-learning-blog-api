cf92d901c186614718a0010d468aaea566978143
SatishPatil <satishsp1625@gmail.com>
2022-08-07 12:30:40 +0530

    Added MONGO DB Connections

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 
 //Namespace import or package for express
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 //object creations
-const app=express();
+const app = express();
 
 //API Configurations
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+//connect to MONGODB
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => {
+    console.log("Connected to MONGO DB Successfully")
+}).catch((error) => {
+    console.log(`Error conecting to MONGO DB ${error.message}`)
+})
 
 //services and endpoint
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API is running Successfully");
 })
 
 //Enable Port
-const PORT=process.env.PORT||1212
+const PORT = process.env.PORT || 1212
 
 //start Server
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server started with port ${PORT}`);
 })
 
